afc34ca4c80c2d596d3773af1590fcdef119ba10
mattw44 <supik713@gmail.com>
2025-05-22 10:48:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,3 @@ st.set_page_config(page_title="Aplikasi Multi-Halaman", layout="centered")
 
 st.title("Selamat Datang di Aplikasi Streamlit")
 st.write("Gunakan menu di sebelah kiri untuk navigasi antar halaman.")
-st.sideheader("Main Menu")
